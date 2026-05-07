8d4cda4dd69eb2bb8528b7da1e0ed255d1906817
Ankit Sharma <ankitsharma.otusone@gmail.com>
2026-05-07 18:30:04 +0530

    post contoller fix

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -83,3 +83,34 @@ export const getPosts = async (
     });
   }
 };
+
+export const likePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({
+                message: "Post not found",
+            });
+        }
+
+        const alreadyLiked = post.likes.includes(req.user._id);
+
+        if (alreadyLiked) {
+            post.likes = post.likes.filter(
+                (id) =>
+                    id.toString() !== req.user._id.toString()
+            );
+        } else {
+            post.likes.push(req.user._id);
+        }
+
+        await post.save();
+
+        res.json(post);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
