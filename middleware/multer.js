175cbb7141ce79565d4f236b95e5543129f51412
Ankit Sharma <ankitsharma.otusone@gmail.com>
2026-05-07 18:17:04 +0530

    multer fix

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,29 +1,60 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+// CREATE UPLOADS FOLDER IF NOT EXISTS
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            Date.now() + path.extname(file.originalname)
-        );
-    },
+  destination: function (
+    req,
+    file,
+    cb
+  ) {
+    cb(null, "uploads/");
+  },
+
+  filename: function (
+    req,
+    file,
+    cb
+  ) {
+    cb(
+      null,
+      Date.now() +
+        path.extname(
+          file.originalname
+        )
+    );
+  },
 });
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-        cb(null, true);
-    } else {
-        cb(new Error("Only images are allowed"), false);
-    }
+const fileFilter = (
+  req,
+  file,
+  cb
+) => {
+  if (
+    file.mimetype.startsWith(
+      "image/"
+    )
+  ) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error(
+        "Only images are allowed"
+      ),
+      false
+    );
+  }
 };
 
 const upload = multer({
-    storage,
-    fileFilter,
+  storage,
+  fileFilter,
 });
 
 export default upload;
